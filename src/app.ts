49ab5b3e9e627d60382d8ed6ce7b9770c88d657a
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-06-26 02:03:52 +0600

    created register api

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import config from "./config";
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import { prisma } from "./lib/prisma";
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 
 const app: Application = express()
 
@@ -17,6 +19,19 @@ app.use(cookieParser())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World")
+})
+app.post("/api/users/register", async (req: Request, res: Response) => {
+    const {name, email, password, profilePhoto} = req.body
+    const isUserExists = await prisma.user.findUnique({
+        where: {email}
+    })
+    if(isUserExists){
+        throw new Error("user with this email already exists")
+    }
+
+    const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds))
+
+    res.status(httpStatus.CREATED).json({message: "register"})
 })
 
 export default app
