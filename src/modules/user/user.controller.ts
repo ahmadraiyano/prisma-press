edd7133803b631219993164c5cfed42383e49cae
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-07-01 01:16:18 +0600

    Get User Profile Based On Logged in User

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,9 @@ import httpStatus from "http-status"
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import jwt from "jsonwebtoken"
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 
 
@@ -60,7 +63,24 @@ const registerUser = catchAsync(async (req: Request, res: Response, next: NextFu
 })
 
 const getMyProfile = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
-    
+    const {accessToken} = req.cookies
+    console.log(accessToken);
+
+    const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret)
+
+    if(typeof verifiedToken === "string"){
+        throw new Error(verifiedToken)
+    }
+    const profile = await userService.getMyProfileFromDB(verifiedToken.id)
+
+    console.log(verifiedToken);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "profile fetched successfully",
+        data: {profile}
+    })
 })
 
 export const userController = {
